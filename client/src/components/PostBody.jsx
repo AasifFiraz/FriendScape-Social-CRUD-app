6f78f5ce5783d2957cc56e673eae3a5c57566d62
AasifFiraz <aasiffiraz178@gmail.com>
2022-12-14 01:07:11 +0530

    Added change to display delete button for the user who created the specific post

--- client/src/components/PostBody.jsx
+++ client/src/components/PostBody.jsx
@@ -4,21 +4,21 @@ import { useMutation } from "@apollo/client";
 import { Button, Card, Image, Icon, Label } from "semantic-ui-react";
 import UserImage from "../assets/user.png";
 import moment from "moment";
-import { LIKE_POST } from "../graphql/Mutation"
+import { LIKE_POST } from "../graphql/Mutation";
+import { useHistory } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth";
 
 const PostBody = ({
-  post: { body, id, username, createdAt, likesCount, commentsCount },
+  post: { body, id, username, user: userId, createdAt, likesCount, commentsCount },
 }) => {
-  const [addLike, {data, loading, error}] = useMutation(LIKE_POST);
-
+  const [addLike, { data, loading, error }] = useMutation(LIKE_POST);
+  const history = useHistory();
+  const context = useContext(AuthContext);
   const likePost = () => {
-    addLike({ variables: { PostId: id } })
-    console.log(data, loading, error)
-  }
-
-  const commentOnPost = () => {
-    console.log("Comment Post")
-  }
+    addLike({ variables: { PostId: id } });
+    console.log(data, loading, error);
+  };
 
   return (
     <Card.Group>
@@ -27,11 +27,11 @@ const PostBody = ({
           <Image floated="right" size="mini" src={UserImage} />
           <Card.Header>{username}</Card.Header>
           <Card.Meta className="post-date" as={Link} to={`/posts/${id}`}>
-            {moment(createdAt).startOf("hour").fromNow()}
+            {moment(createdAt).fromNow()}
           </Card.Meta>
           <Card.Description>{body}</Card.Description>
         </Card.Content>
-        <Card.Content>
+        <Card.Content textAlign="center">
           <Button as="div" labelPosition="right" onClick={likePost}>
             <Button basic color="red">
               <Icon name="heart" />
@@ -40,7 +40,11 @@ const PostBody = ({
               {likesCount}
             </Label>
           </Button>
-          <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button
+            as="div"
+            labelPosition="right"
+            onClick={() => history.push(`/posts/${id}`)}
+          >
             <Button basic color="blue">
               <Icon name="comment" />
             </Button>
@@ -48,6 +52,11 @@ const PostBody = ({
               {commentsCount}
             </Label>
           </Button>
+          {context.user && context.user.id === userId && (
+            <Button style={{marginLeft: '20px'}} color="red">
+              <Icon name="trash" />
+            </Button>
+          )}
         </Card.Content>
       </Card>
     </Card.Group>
