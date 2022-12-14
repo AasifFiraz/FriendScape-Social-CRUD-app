905a77a989c611f7e62f54b4ae780aadc765a5f3
AasifFiraz <aasiffiraz178@gmail.com>
2022-12-15 00:00:22 +0530

    Changed port name and fixed npm start

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req }),
 });
 
-const PORT = process.env.port || 4000
+const PORT = process.env.PORT || 4000
 
 const start = async () => {
   try {
